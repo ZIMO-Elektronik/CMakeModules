064447726f9c23b6f3779af972d08c0ac70fd087
Vincent Hamp <vincent.hamp@higaski.at>
2023-07-14 11:27:11 +0200

    Add a whole bunch of -Werror flags to target_common_warnings

--- src/modules/target_common_warnings.cmake
+++ src/modules/target_common_warnings.cmake
@@ -25,7 +25,7 @@ SOFTWARE.
 function(target_common_warnings)
   # Set common warnings
   set(C_CXX_WARNINGS
-      -Wall;-Wextra;-Wshadow;-Wunused;-Wcast-align;-Wpedantic;-Wconversion;-Wsign-conversion;-Wmisleading-indentation;-Wnull-dereference;-Wdouble-promotion;-Wfatal-errors
+      -Wall;-Wextra;-Wshadow;-Wunused;-Wcast-align;-Wpedantic;-Wconversion;-Wsign-conversion;-Wmisleading-indentation;-Wnull-dereference;-Wdouble-promotion;-Wfatal-errors;-Werror=array-bounds;-Werror=array-compare;-Werror=dangling-else;-Werror=implicit-fallthrough;-Werror=parentheses;-Werror=infinite-recursion;-Werror=logical-not-parentheses;-Werror=misleading-indentation;-Werror=return-type;-Werror=sequence-point;-Werror=shift-negative-value;-Werror=shift-overflow;-Werror=sizeof-array-div;-Werror=strict-aliasing;-Werror=tautological-compare;-Werror=type-limits
   )
   set(CXX_WARNINGS
       "$<$<COMPILE_LANGUAGE:CXX>:-Wnon-virtual-dtor;-Wold-style-cast;-Wsuggest-override;-Woverloaded-virtual>"
